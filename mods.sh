3fc5dd2992d3471d70a2abdb74332355520c4786
Ashutosh Tripathi <pushpatripathi1111@gmail.com>
2022-06-28 15:45:45 +0530

    Update mods.sh

--- mods.sh
+++ mods.sh
@@ -1,6 +1,6 @@
 modprobe i915 disable_display:N
 modprobe i915 disable_power_well:-1
-modprobe i915 dmc_firmware_path:(null)
+modprobe i915 dmc_firmware_path
 modprobe i915 edp_vswing:0
 modprobe i915 enable_dc:-1
 modprobe i915 enable_dpcd_backlight:-1
@@ -15,9 +15,9 @@ modprobe i915 enable_psr2_sel_fetch:Y
 modprobe i915 parameters/error_capture:Y
 modprobe i915 fastboot:-1
 modprobe i915 force_reset_modeset_test:N
-modprobe i915 guc_firmware_path:(null)
+modprobe i915 guc_firmware_path
 modprobe i915 guc_log_level:-1
-modprobe i915 huc_firmware_path:(null)
+modprobe i915 huc_firmware_path
 modprobe i915 invert_brightness:0
 modprobe i915 load_detect_test:N
 modprobe i915 lvds_channel_mode:0
@@ -30,6 +30,6 @@ modprobe i915 panel_use_ssc:-1
 modprobe i915 psr_safest_params:N
 modprobe i915 request_timeout_ms:20000
 modprobe i915 reset:3
-modprobe i915 vbt_firmware:(null)
+modprobe i915 vbt_firmware
 modprobe i915 vbt_sdvo_panel_type:-1
 modprobe i915 verbose_state_checks:Y
